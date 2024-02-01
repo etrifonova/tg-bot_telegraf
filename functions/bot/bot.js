3a16f74c7b2b3d143c9fc4980afbf8deecc6691f
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-02-01 19:55:59 +0300

    fix import

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,44 +1,111 @@
-const { Telegraf } = require("telegraf")
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 // const questionsGeography = require("../../questions");
 
 const questionsGeography = [
-    {question: "Столица Мадагаскара?", answer: "Антананариву"},
-    {question: "В какой стране находится Петра?", answer: "Иордания"},
-    {question: "На каком языке говорят в Либерии?", answer: "английский"},
-    {question: "Какой горный хребет является самым высоким на Земле?", answer: "Гималаи"},
-    {question: "В какой стране находится Сахара, крупнейшая пустыня в мире?", answer: "Алжир"},
-    {question: "Какое море омывает северную часть Африки?", answer: "Средиземное"},
-    {question: "Какая страна занимает большую часть скандинавского полуострова?", answer: "Норвегия"},
-    {question: "Какой город считается самым населенным в мире?", answer: "Токио"},
-    {question: "Какая река является самой длинной в мире?", answer: "Амазонка"},
-    {question: "Какое государство является крупнейшим по площади в Африке?", answer: "Алжир"},
-    {question: "Какое озеро является самым крупным по площади в Северной Америке?", answer: "Супериор"},
-    {question: "В какой стране находятся Гималаи?", answer: "Непал"},
-    {question: "Какое море отделяет Аравийский полуостров от Индийского субконтинента?", answer: "Аравийское"},
-    {question: "Какой из островов Карибского бассейна является наименьшим по площади?", answer: "Сент-Китс"},
-    {question: "Какой город является самым северным столичным городом в мире?", answer: "Рейкьявик"},
-    {question: "В какой стране находится самый высокогорный город планеты?", answer: "Боливия"},
-    {question: "В какой стране находится самый глубокий каньон в мире, Котауаси (глубина 3535 метров)?", answer: "Перу"},
-    {question: "Какое море отделяет Сицилию от Италии?", answer: "Тирренское"},
-    {question: "Где находится Гринвичская часовня, определяющая нулевой меридиан?", answer: "Лондон"},
-    {question: "Какое государство считается самым малонаселенным в мире?", answer: "Монако"},
-    {question: "Какой водный путь соединяет Атлантический и Тихий океаны через Центральную Америку?", answer: "Панамский канал"},
-    {question: "В какой стране находится самое высокое поселение на планете, Ла Ринконада?", answer: "Перу"},
-    {question: "На границе каких двух стран находится самый высокий вулкан в мире Охос-дель-Саладо, расположенный на высоте 6893 метров? Указывайте страны подряд, в алфавитном порядке, без запятой.", answer: "Аргентина Чили"},
-    {question: "Какое озеро считается самым глубоким в Африке (макс. глубина: 1471 метр)?", answer: "Танганьика"},
-    {question: "Бассейн какой реки считается самым крупным в мире по площади водосбора?", answer: "Амазонка"}
-]
+  { question: "Столица Мадагаскара?", answer: "Антананариву" },
+  { question: "В какой стране находится Петра?", answer: "Иордания" },
+  { question: "На каком языке говорят в Либерии?", answer: "английский" },
+  {
+    question: "Какой горный хребет является самым высоким на Земле?",
+    answer: "Гималаи",
+  },
+  {
+    question: "В какой стране находится Сахара, крупнейшая пустыня в мире?",
+    answer: "Алжир",
+  },
+  {
+    question: "Какое море омывает северную часть Африки?",
+    answer: "Средиземное",
+  },
+  {
+    question: "Какая страна занимает большую часть скандинавского полуострова?",
+    answer: "Норвегия",
+  },
+  {
+    question: "Какой город считается самым населенным в мире?",
+    answer: "Токио",
+  },
+  { question: "Какая река является самой длинной в мире?", answer: "Амазонка" },
+  {
+    question: "Какое государство является крупнейшим по площади в Африке?",
+    answer: "Алжир",
+  },
+  {
+    question:
+      "Какое озеро является самым крупным по площади в Северной Америке?",
+    answer: "Супериор",
+  },
+  { question: "В какой стране находятся Гималаи?", answer: "Непал" },
+  {
+    question:
+      "Какое море отделяет Аравийский полуостров от Индийского субконтинента?",
+    answer: "Аравийское",
+  },
+  {
+    question:
+      "Какой из островов Карибского бассейна является наименьшим по площади?",
+    answer: "Сент-Китс",
+  },
+  {
+    question: "Какой город является самым северным столичным городом в мире?",
+    answer: "Рейкьявик",
+  },
+  {
+    question: "В какой стране находится самый высокогорный город планеты?",
+    answer: "Боливия",
+  },
+  {
+    question:
+      "В какой стране находится самый глубокий каньон в мире, Котауаси (глубина 3535 метров)?",
+    answer: "Перу",
+  },
+  { question: "Какое море отделяет Сицилию от Италии?", answer: "Тирренское" },
+  {
+    question:
+      "Где находится Гринвичская часовня, определяющая нулевой меридиан?",
+    answer: "Лондон",
+  },
+  {
+    question: "Какое государство считается самым малонаселенным в мире?",
+    answer: "Монако",
+  },
+  {
+    question:
+      "Какой водный путь соединяет Атлантический и Тихий океаны через Центральную Америку?",
+    answer: "Панамский канал",
+  },
+  {
+    question:
+      "В какой стране находится самое высокое поселение на планете, Ла Ринконада?",
+    answer: "Перу",
+  },
+  {
+    question:
+      "На границе каких двух стран находится самый высокий вулкан в мире Охос-дель-Саладо, расположенный на высоте 6893 метров? Указывайте страны подряд, в алфавитном порядке, без запятой.",
+    answer: "Аргентина Чили",
+  },
+  {
+    question:
+      "Какое озеро считается самым глубоким в Африке (макс. глубина: 1471 метр)?",
+    answer: "Танганьика",
+  },
+  {
+    question:
+      "Бассейн какой реки считается самым крупным в мире по площади водосбора?",
+    answer: "Амазонка",
+  },
+];
 
-bot.start(ctx => {
-  console.log("It's alive!")
+bot.start((ctx) => {
+  console.log("It's alive!");
   try {
-    return ctx.reply("Hello there")
+    return ctx.reply("Hello there");
   } catch (e) {
-    console.error("error in start action:", e)
-    return ctx.reply("Error occured")
+    console.error("error in start action:", e);
+    return ctx.reply("Error occured");
   }
-})
+});
 
 let randomElement;
 function generateQuestion() {
@@ -49,57 +116,46 @@ function generateQuestion() {
 }
 
 bot.command("geography", (ctx) => {
-    if (questionsGeography.length > 0) {
-      generateQuestion();
-      ctx.reply(randomElement.question);
-      questionsGeography.splice(questionsGeography.indexOf(randomElement), 1);
-    } else ctx.reply("Вопросы закончились");
-  });
-  
-  bot.on("message", (ctx) => {
-    if (ctx.message.text === randomElement.answer) {
-      ctx.reply("Верно!");
-      questionsGeography.splice(questionsGeography.indexOf(randomElement), 1);
-    //   if (questionsGeography.length > 0) {
-    //     generateQuestion();
-    //     setTimeout(() => {
-    //       ctx.reply(randomElement.question);
-    //   }, 1000)
-            
-    //     } else {
-    //       ctx.reply("Вопросы закончились")  
-    //     }
-      }    
-      else if (ctx.message.text === "/ответ") {
-      ctx.reply(randomElement.answer);
-      generateQuestion();
-      setTimeout(() => {
-        ctx.reply(randomElement.question);
-    }, 1000)
-    } else {
-      ctx.reply("Неверно!");
-    }
-  });
+  if (questionsGeography.length > 0) {
+    generateQuestion();
+    ctx.reply(randomElement.question);
+    questionsGeography.splice(questionsGeography.indexOf(randomElement), 1);
+  } else ctx.reply("Вопросы закончились");
+});
 
-  bot.command("next", (ctx) => {
+bot.on("message", (ctx) => {
+  if (ctx.message.text === randomElement.answer) {
+    ctx.reply("Верно!");
+    questionsGeography.splice(questionsGeography.indexOf(randomElement), 1);
     if (questionsGeography.length > 0) {
       generateQuestion();
       setTimeout(() => {
         ctx.reply(randomElement.question);
-    }, 1000)
-          
-      } else {
-        ctx.reply("Вопросы закончились")  
-      }
-  });
+      }, 1000);
+    } else {
+      ctx.reply("Вопросы закончились");
+    }
+  } else if (ctx.message.text === "/ответ") {
+    ctx.reply(randomElement.answer);
+    generateQuestion();
+    setTimeout(() => {
+      ctx.reply(randomElement.question);
+    }, 1000);
+  } else {
+    ctx.reply("Неверно!");
+  }
+});
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-exports.handler = async event => {
+exports.handler = async (event) => {
   try {
-    await bot.handleUpdate(JSON.parse(event.body))
-    return { statusCode: 200, body: "" }
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
   } catch (e) {
-    console.error("error in handler:", e)
-    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+    console.error("error in handler:", e);
+    return {
+      statusCode: 400,
+      body: "This endpoint is meant for bot and telegram communication",
+    };
   }
-}
+};
