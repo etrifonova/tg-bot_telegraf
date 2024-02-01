9361a7dde0e9af59bf018941faf46a5beb7c06bd
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-02-01 19:33:36 +0300

    add questions

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require("telegraf")
 const bot = new Telegraf(process.env.BOT_TOKEN)
+import { questionsGeography } from "./questions.js";
 
 bot.start(ctx => {
   console.log("It's alive!")
@@ -10,6 +11,47 @@ bot.start(ctx => {
     return ctx.reply("Error occured")
   }
 })
+
+let randomElement;
+function generateQuestion() {
+  randomElement = questionsGeography.map((element) => element)[
+    Math.floor(Math.random() * questionsGeography.length)
+  ];
+  return randomElement;
+}
+
+bot.command("geography", (ctx) => {
+    if (questionsGeography.length > 0) {
+      generateQuestion();
+      ctx.reply(randomElement.question);
+      questionsGeography.splice(questionsGeography.indexOf(randomElement), 1);
+    } else ctx.reply("Вопросы закончились");
+  });
+  
+  bot.on("message", (ctx) => {
+    if (ctx.message.text === randomElement.answer) {
+      ctx.reply("Верно!");
+      questionsGeography.splice(questionsGeography.indexOf(randomElement), 1);
+      if (questionsGeography.length > 0) {
+        generateQuestion();
+        setTimeout(() => {
+          ctx.reply(randomElement.question);
+      }, 1000)
+            
+        } else {
+          ctx.reply("Вопросы закончились")  
+        }
+      }    
+      else if (ctx.message.text === "/ответ") {
+      ctx.reply(randomElement.answer);
+      generateQuestion();
+      setTimeout(() => {
+        ctx.reply(randomElement.question);
+    }, 1000)
+    } else {
+      ctx.reply("Неверно!");
+    }
+  });
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 exports.handler = async event => {
