374d30f4fe35a3102b5edd7eedacbd9fc966cea7
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-02-19 16:01:36 +0300

    add questions

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -41,6 +41,76 @@ const questions = [
       "Операция “Семь Поттеров”: с кем летел Фред? (напишите имя и фамилию в именительном падеже)",
     answer: "Артур Уизли",
   },
+  {
+    question:
+      "Битва за Хогвартс: против кого сражался Хагрид? (имя и фамилия в именительном падеже)",
+    answer: "Уолден Макнейр",
+  },
+  {
+    question:
+      "Битва за Хогвартс: против кого сражался Флитвик (имя и фамилия в именительном падеже)",
+    answer: "Антонин Долохов",
+  },
+  {
+    question:
+      "Битва за Хогвартс: против кого сражался Аберфорт (имя и фамилия в именительном падеже)",
+    answer: "Август Руквуд",
+  },
+  {
+    question:
+      "Битва за Хогвартс: против кого сражались Перси и Артур (имя и фамилия в именительном падеже)",
+    answer: "Пий Толстоватый",
+  },
+  {
+    question:
+      "С какими пожирателями смерти сражались Гарри, Рон и Гермиона в кафе в Лондоне? (имя и фамилия в именительном падеже, в алфавитном порядке)",
+    answer: "Антонин Долохов, Торфин Роули",
+  },
+  {
+    question:
+      "Какой сюрприз Дадли приготовил для Гарри и поставил за дверью его комнаты? Ответьте двумя словами на одну и ту же букву, в именительном падеже.",
+    answer: "Чашка чая",
+  },
+  {
+    question:
+      "С чем Рита Скиттер сравнила информацию о дуэли Дамблдора с Грин-де-Вальдом? Ответьте тремя словами, два из которых начинаются на соседние согласные буквы; главное слово в именительном падеже",
+    answer: "Взрыв навозной бомбы",
+  },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
+  // {
+  //   question:
+  //     "",
+  //   answer: "",
+  // },
 ];
 
 let randomElement;
@@ -72,7 +142,7 @@ bot.command("question", (ctx) => {
 bot.on("message", (ctx) => {
   if (
     questionsPotter.length == 1 &&
-    ctx.message.text === randomElement.answer
+    ctx.message.text.trim().toLowerCase() === randomElement.answer.trim().toLowerCase()
   ) {
     questionsPotter.splice(questionsPotter.indexOf(randomElement), 1);
     randomElement = generateQuestion();
@@ -81,7 +151,7 @@ bot.on("message", (ctx) => {
     ctx.reply("Верно! \n\n Это был последний вопрос.");
     randomElement = generateQuestion();
   }
-  else if (questionsPotter.length > 1 && ctx.message.text === randomElement.answer) {
+  else if (questionsPotter.length > 1 && ctx.message.text.trim().toLowerCase() === randomElement.answer.trim().toLowerCase()) {
     questionsPotter.splice(questionsPotter.indexOf(randomElement), 1);
     randomElement = generateQuestion();
     console.log("Осталось вопросов:" + questionsPotter.length);
